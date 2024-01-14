b42639108c6e0e101efce6f3cd9b2b2d9f86291d
Blx <itszacktouil@gmail.com>
2024-01-14 21:00:48 +0100

    'update'

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const express_useragent_1 = __importDefault(require("express-useragent"));
 const requestIp = __importStar(require("request-ip"));
 const cors_1 = __importDefault(require("cors"));
 const axios_1 = __importDefault(require("axios"));
+const js_sha256_1 = require("js-sha256");
 const Content = bizSdk.Content;
 const CustomData = bizSdk.CustomData;
 const DeliveryCategory = bizSdk.DeliveryCategory;
@@ -89,8 +90,57 @@ app.post("/fb-api", (0, express_async_handler_1.default)(async (req, res) => {
         res.status(505).json({ status: "error", message: "somthing wrong", error: error.message });
     }
 }));
-app.post('/tiktopApi', (0, express_async_handler_1.default)(async (req, res) => {
-    await axios_1.default.post('https://business-api.tiktok.com/open_api/v1.2/pixel/track/');
+app.post("/tiktopApi", (0, express_async_handler_1.default)(async (req, res) => {
+    const headers = {
+        "Access-Token": req.body.access_token,
+        "Content-Type": "application/json",
+    };
+    console.log(`${req.protocol}://${req.hostname}`);
+    let data = {
+        pixel_code: req.body.pixelId,
+        event: "CompletePayment",
+        event_id: `${req.body.productId}${req.body.phoneNumber}`,
+        timestamp: new Date(),
+        context: {
+            page: {
+                url: req.body.eventSourceUrl,
+                referrer: `${req.protocol}://${req.hostname}`,
+            },
+            user: {
+                external_id: (0, js_sha256_1.sha256)(`${req.body.productId}${req.body.phoneNumber}`),
+                phone_number: (0, js_sha256_1.sha256)(`+${req.body.phoneNumber}`),
+            },
+            user_agent: req.body.userAgent,
+            ip: req.clientIp,
+        },
+        properties: {
+            contents: [
+                {
+                    price: req.body.productPrice,
+                    quantity: req.body.quantity,
+                    content_type: "product",
+                    content_id: req.body.productId,
+                    content_name: req.body.productName
+                },
+            ],
+            currency: req.body.currencyCode,
+            value: req.body.totalPrice,
+        },
+    };
+    //@ts-ignore
+    if (req.body.ttclid)
+        data = { ...data, context: { ...data.context, ad: { callback: req.body.ttclid } } };
+    //@ts-ignore
+    if (req.body.ttp)
+        data = { ...data, context: { ...data.context, user: { ...data.context.user, ttp: req.body.ttp } } };
+    try {
+        await axios_1.default.post("https://business-api.tiktok.com/open_api/v1.2/pixel/track/", data, { headers });
+        res.json({ status: "succes" });
+    }
+    catch (_error) {
+        const error = _error;
+        res.status(505).json({ status: "error", message: "somthing wrong", error: error.message });
+    }
 }));
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
