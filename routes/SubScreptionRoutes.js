25a353b439e629ce85b3ebc90f8d852385f869c0
Blx <itszacktouil@gmail.com>
2024-01-16 09:16:33 +0100

    'update'

--- routes/SubScreptionRoutes.js
+++ routes/SubScreptionRoutes.js
@@ -8,6 +8,6 @@ const express_1 = __importDefault(require("express"));
 const SubScreptionService_1 = require("../services/SubScreptionService");
 const router = express_1.default.Router();
 router.route('/').get(SubScreptionService_1.getAllSubScreptions).post(SubScreptionService_1.createNewSubScreption);
-router.route('/veirfy/:hostName').get(SubScreptionService_1.getSubScreptionByHostName);
+router.route('/verify/:hostName').get(SubScreptionService_1.getSubScreptionByHostName);
 router.route('/:id').get(SubScreptionService_1.getSubScreptionById);
 exports.subScreptionRoutes = router;
